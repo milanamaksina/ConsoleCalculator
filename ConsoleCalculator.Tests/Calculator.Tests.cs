e7cebeb611dc4eb0a84acc4d2cc3c427ea235ff9
Milana Maksina <m_maksinaa@mail.ru>
2022-08-10 22:25:29 +0300

    feature: add positive tests for DoOperation

--- ConsoleCalculator.Tests/Calculator.Tests.cs
+++ ConsoleCalculator.Tests/Calculator.Tests.cs
@@ -1,11 +1,63 @@
 namespace ConsoleCalculator.Tests
 {
-    public class UnitTest1
+    public class CalculatorTests
     {
-        [Fact]
-        public void Test1()
-        {
 
+        public const string multiplyOperation = "m";
+        public const string addingOperation = "a";
+        public const string subtractionOperation = "s";
+        public const string divideOperation = "d";
+
+
+
+        [Fact]
+        public void DoOperationMultiplyTest()
+        {
+            double a = 10;
+            double b = 20;
+            double expectedResult = 200;
+
+            double actualResult = Calculator.DoOperation(a, b, multiplyOperation);
+
+            Assert.Equal(expectedResult, actualResult);
         }
+
+        [Fact]
+        public void DoOperationAddTest()
+        {
+            double a = -5;
+            double b = 7;
+            double expectedResult = 2;
+
+            double actualResult = Calculator.DoOperation(a, b, addingOperation);
+
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void DoOperationSubtractTest()
+        {
+            double a = 1000;
+            double b = 7;
+            double expectedResult = 993;
+
+            double actualResult = Calculator.DoOperation(a, b, subtractionOperation);
+
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void DoOperationDivideTest()
+        {
+            double a = 1000;
+            double b = 10;
+            double expectedResult = 100;
+
+            double actualResult = Calculator.DoOperation(a, b, divideOperation);
+
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+       
     }
 }
