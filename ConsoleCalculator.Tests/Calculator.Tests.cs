ba5fa8953f444d25254145412bca8bcc91bbeeb2
Milana Maksina <m_maksinaa@mail.ru>
2022-08-11 20:01:25 +0300

    feature: add console test ShouldReturnResultOfAdding

--- ConsoleCalculator.Tests/Calculator.Tests.cs
+++ ConsoleCalculator.Tests/Calculator.Tests.cs
@@ -1,3 +1,8 @@
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Program;
+using System.Text;
+using System.Text.RegularExpressions;
+
 namespace ConsoleCalculator.Tests
 {
     public class CalculatorTests
@@ -11,7 +16,7 @@ namespace ConsoleCalculator.Tests
 
 
         [Fact]
-        public void DoOperationMultiplyTest()
+        public void ShouldMultiplyTest()
         {
             double a = 10;
             double b = 20;
@@ -23,7 +28,7 @@ namespace ConsoleCalculator.Tests
         }
 
         [Fact]
-        public void DoOperationAddTest()
+        public void ShoiuldAddTest()
         {
             double a = -5;
             double b = 7;
@@ -35,7 +40,7 @@ namespace ConsoleCalculator.Tests
         }
 
         [Fact]
-        public void DoOperationSubtractTest()
+        public void ShouldSubtractTest()
         {
             double a = 1000;
             double b = 7;
@@ -47,7 +52,7 @@ namespace ConsoleCalculator.Tests
         }
 
         [Fact]
-        public void DoOperationDivideTest()
+        public void ShouldDoDivideTest()
         {
             double a = 1000;
             double b = 10;
@@ -58,15 +63,49 @@ namespace ConsoleCalculator.Tests
             Assert.Equal(expectedResult, actualResult);
         }
         [Fact]
-        public void DoOperationDivideTest_WhenAddedWrongCommand()
+        public void ShouldDivideTest_WhenAddedEmpltyCommand()
         {
             double a = 100;
             double b = 223;
             var expectedResult = double.NaN;
-
             double actualResult = Calculator.DoOperation(a, b, null);
 
             Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void ShouldReturnResultOfAdding()
+        {
+            var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+
+            var stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("4");
+            stringBuilder.AppendLine("5");
+            stringBuilder.AppendLine("m");
+            stringBuilder.AppendLine("n");
+            var stringReader = new StringReader(stringBuilder.ToString());
+            Console.SetIn(stringReader);
+
+
+
+            MainProgram.Main(new string[0]);
+            var expectedResult = "Console Calculator in C#" +
+                                 "------------------------" +
+                                 "Type a number, and then press Enter: " +
+                                 "Type another number, and then press Enter: " +
+                                 "Choose an operator from the following list:" +
+                                 "a - Add" +
+                                 "s - Subtract" +
+                                 "m - Multiply" +
+                                 "d - Divide" +
+                                 "Your option? " +
+                                 "Your result: 20" +
+                                 "------------------------" +
+                                 "Press 'n' and Enter to close the app, or press any other key and Enter to continue: ";
+
+            Assert.Equal(expectedResult, Regex.Replace(stringWriter.ToString(), @"[\r\t\n]+", string.Empty));
+
         }
     }
 }
